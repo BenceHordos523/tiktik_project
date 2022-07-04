59e5d888ce16f76cb930635ec8b0feffa214506d
BenceHordos523 <Bence.Hordos523@gmail.com>
2022-07-04 21:54:19 +0200

    Created main dashboard and login-logout functionality

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["kajabi-storefronts-production.kajabi-cdn.com", "lh3.googleusercontent.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
